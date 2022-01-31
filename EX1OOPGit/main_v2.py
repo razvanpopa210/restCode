113f2373951eabf5af8b5ff744165db6c8797108
Popa Razvan-George <razvanpopa210@gmail.com>
2022-01-31 12:00:03 +0200

    second try

--- EX1OOPGit/main_v2.py
+++ EX1OOPGit/main_v2.py
@@ -6,13 +6,25 @@ import users
 import posts
 import comments
 import todos
+import tomlintegrator
+import tom22
+path = 'tomlfiles/users.toml'
 
+# new_user_one = users.Users('Test1', 'test1@popa.com', 'male', 'active', '999')
+# new_user_one.post_method()
+# new_user_two = users.Users('Test2', 'test2@popa.com', 'male', 'active')
+# new_user_two.post_method()
+# new_post = posts.Posts('TestPost', 'TestMain', '2544')
+# new_post.post_method()
+# print(one_Object.idsREST['posts'])
 
-
-new_user_one = users.Users('Test1', 'test1@popa.com', 'male', 'active', '999')
-new_user_one.post_method()
-new_user_two = users.Users('', 'Test2', 'test2@popa.com', 'male', 'active')
-new_user_two.post_method()
-one_Object = methods.methods()
+# new_comm = comments.Comments('best', 'best@email', 'bodybody', '1346')
+# new_comm.post_method()
 # print(one_Object.idsREST)
-one_Object.delete_all_v2()
+# path = 'tomlfiles/users.toml'
+#
+one_Object = methods.methods()
+inte = tom22.Integrator(path)
+
+inte.analyse_data()
+one_Object.delete_all_v3()
--- EX1OOPGit/posts.py
+++ EX1OOPGit/posts.py
@@ -10,26 +10,29 @@ URL = 'https://gorest.co.in/public/v1/'
 
 class Posts(methods.methods):
 
-    def __init__(self, title, body, post_id=None):
+    def __init__(self, title, body, user_id = None, post_id=None):
 
         super().__init__()
-        self.id = post_id
+        self.user_id = user_id
+        self.post_id = post_id
         self.title = title
         self.body = body
 
     def get_setter(self):
-        if self.id:
-            self.url = 'https://gorest.co.in/public/v1/posts/' + self.id
+        if self.post_id:
+            self.url = 'https://gorest.co.in/public/v1/posts/' + self.post_id
         else:
             self.url = 'https://gorest.co.in/public/v1/posts/'
 
+
     def post_setter(self):
-        data = {
+        self.data = {
             'title':self.title,
             'body':self.body
         }
+
         self.tp = 'posts'
-        self.url = 'https://gorest.co.in/public/v1/users/' + self.id + '/posts'
+        self.url = 'https://gorest.co.in/public/v1/users/' + self.user_id + '/posts'
 
     def del_setter(self):
         self.url = 'https://gorest.co.in/public/v1/posts/' + self.id
